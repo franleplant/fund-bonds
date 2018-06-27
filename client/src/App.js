ec1cd47b4ad89f7a0c508b7cafa0692c576e9a76
Fran Guijarro <franleplant@gmail.com>
2018-06-27 20:36:10 -0300

    display a chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import moment from "moment";
 import "./App.css";
+import Chart from "./Chart";
 
+
+const COLORS = [
+
+
+]
 async function getData() {
   const res = await fetch("api/data", {
     accept: "application/json"
@@ -9,7 +16,82 @@ async function getData() {
   return res.json();
 }
 
-class App extends React.Component {
+function charRange(charA, charZ) {
+  const a = [];
+  let i = charA.charCodeAt(0);
+  const j = charZ.charCodeAt(0);
+
+  for (; i <= j; ++i) {
+    a.push(String.fromCharCode(i));
+  }
+
+  return a;
+}
+
+function transform(cells) {
+  const DATA_START_ROW = 8;
+  const HEADERS_ROW = 7;
+  const DEFAULT_OPTS = {
+    fill: false,
+    lineTension: 0.1,
+    backgroundColor: "rgba(75,192,192,0.4)",
+    borderColor: "rgba(75,192,192,1)",
+    borderCapStyle: "butt",
+    borderDash: [],
+    borderDashOffset: 0.0,
+    borderJoinStyle: "miter",
+    pointBorderColor: "rgba(75,192,192,1)",
+    pointBackgroundColor: "#fff",
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    pointHoverBackgroundColor: "rgba(255,0,0,1)",
+    pointHoverBorderColor: "rgba(220,220,220,1)",
+    pointHoverBorderWidth: 2,
+    pointRadius: 1,
+    pointHitRadius: 10
+  };
+
+  const dates = [];
+  const datasets = {};
+  charRange("B", "T").forEach(colIndex => {
+    const header = cells[`${colIndex}${HEADERS_ROW}`].v;
+
+    datasets[colIndex] = {
+      label: header,
+      data: [],
+      ...DEFAULT_OPTS
+    };
+  });
+
+  for (let rowIndex = DATA_START_ROW; ; rowIndex++) {
+    const dateCell = cells[`A${rowIndex}`];
+    if (!dateCell) {
+      break;
+    }
+
+    const date = moment(dateCell.w, "D-MMM-YY").toISOString();
+    dates.push(date);
+
+    charRange("B", "T").forEach(colIndex => {
+      // TODO we might need data transformations in here
+      const cellValue = (cells[`${colIndex}${rowIndex}`] || {}).v || undefined;
+      datasets[colIndex].data.push({
+        x: date,
+        y: cellValue,
+      });
+    });
+  }
+
+  console.log(dates);
+  console.log(datasets);
+
+  return {
+    labels: dates,
+    datasets: Object.values(datasets)
+  };
+}
+
+export default class App extends React.Component {
   state = {
     data: {},
     loading: true
@@ -17,21 +99,27 @@ class App extends React.Component {
 
   async componentDidMount() {
     const res = await getData();
-    this.setState({ data: res, loading: false });
+    const xls = res.xls;
+    const cells = xls.Sheets.Cuotaspartes;
+    this.setState({
+      rawData: xls,
+      data: transform(cells),
+      loading: false
+    });
   }
 
   render() {
     if (this.state.loading) {
       return <p>Loading ...</p>;
     }
+
+    //const generateColor = () => '#'+Math.floor(Math.random()*16777215).toString(16);
+    //const colors = headers.map(generateColor)
+
     return (
       <div>
-        <code>
-          <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
-        </code>
+        <Chart data={this.state.data} />
       </div>
     );
   }
 }
-
-export default App;
---
+++ client/src/Chart.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+//TODO use decimation to reduce the amount of points
+//this lib might work http://mourner.github.io/simplify-js/
+export default class Chart extends React.Component {
+  render() {
+    return (
+      <Line
+        data={this.props.data}
+        options={{
+          cubicInterpolationMode: 'monotone',
+          scales: {
+            xAxes: {
+              type: "time",
+              distribution: "series",
+              time: {
+                tooltipFormat: "YYYY-MM-DD"
+              }
+            }
+          },
+          animation: {
+            duration: 0 // general animation time
+          },
+          hover: {
+            animationDuration: 0 // duration of animations when hovering an item
+          },
+          responsiveAnimationDuration: 0, // animation duration after a resize
+          showLines: false,
+          elements: {
+            line: {
+              tension: 0 // disables bezier curves
+            }
+          }
+        }}
+      />
+    );
+  }
+}
